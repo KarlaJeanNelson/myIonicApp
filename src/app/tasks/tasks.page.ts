4ed98e75b8833478ab1a84b0324131b2d95a717c
KarlaJeanNelson <karla.jean.nelson@gmail.com>
2019-02-12 22:09:36 -0600

    simple reorder list

--- src/app/tasks/tasks.page.ts
+++ src/app/tasks/tasks.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.page.scss'],
 })
 export class TasksPage implements OnInit {
+	items: Array<string>
 
-  constructor() { }
+  constructor(
+	) { }
 
   ngOnInit() {
+		this.items = ["First item", "Second item", "Third item"];
   }
 
+	reorderItems(ev) {
+    const itemMove = this.items.splice(ev.detail.from, 1)[0];
+    this.items.splice(ev.detail.to, 0, itemMove);
+    ev.detail.complete();
+	}
 }
